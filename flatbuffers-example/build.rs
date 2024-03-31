a34afdfd052afebe526c7e88958d7a7e2c689a3a
Ricardo Delfin <me@rdelfin.com>
2024-03-31 03:00:29 +0100

    Moved flatbuffer schemas into folder

--- flatbuffers-example/build.rs
+++ flatbuffers-example/build.rs
@@ -1,7 +1,7 @@
 use flatbuffers_build::BuilderOptions;
 
 fn main() {
-    BuilderOptions::new_with_files(["weapon.fbs", "example.fbs"])
+    BuilderOptions::new_with_files(["schemas/weapon.fbs", "schemas/example.fbs"])
         .set_symlink_directory("src/gen_flatbuffers")
         .compile()
         .expect("flatbuffer compilation failed")
