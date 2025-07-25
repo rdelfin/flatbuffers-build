4ee38a50f888fe03973c194b8594663a2cdf2deb
fliiiix <hi@l33t.name>
2025-07-25 14:27:10 +0200

    Generate Object API by default
    Expanended example with the new Object API.
    This is fully backwards compatible and just added new Object types
    for simpler pack and unpack messages with Flatbuffers.

--- flatbuffers-build-example/src/main.rs
+++ flatbuffers-build-example/src/main.rs
@@ -6,9 +6,10 @@ mod gen_flatbuffers {
     // for a cleaner code, you could do this in a separate module
     include!(concat!(env!("OUT_DIR"), "/flatbuffers/mod.rs"));
 }
-use gen_flatbuffers::my_game::sample::{Monster, MonsterArgs, Vec3};
+use gen_flatbuffers::my_game::sample::{Monster, MonsterArgs, MonsterT, Vec3, WeaponT};
 
 fn main() -> anyhow::Result<()> {
+    // Use builder style:
     // Writing a monster object to encoded bytes
     let mut builder = flatbuffers::FlatBufferBuilder::with_capacity(1024);
     let m = Monster::create(
@@ -23,7 +24,27 @@ fn main() -> anyhow::Result<()> {
 
     // Reading the object back
     let monster = flatbuffers::root::<Monster>(encoded_data)?;
-    println!("Read back monster: {monster:?}");
+    println!("Read back monster (created builder style): {monster:?}");
+
+    // Use Object style:
+    let mut builder = flatbuffers::FlatBufferBuilder::with_capacity(1024);
+    let sword: WeaponT = WeaponT {
+        name: Some("sword".to_string()),
+        damage: 420,
+    };
+    let m: MonsterT = MonsterT {
+        name: Some("Le MonsterT".to_string()),
+        weapons: vec![sword].into(),
+        ..Default::default()
+    };
+
+    let monster_offset = m.pack(&mut builder);
+    builder.finish(monster_offset, None);
+    let encoded_data = builder.finished_data();
+
+    // Reading the object back
+    let monster = flatbuffers::root::<Monster>(encoded_data)?;
+    println!("Read back monster (create with object api): {monster:?}");
 
     Ok(())
 }
